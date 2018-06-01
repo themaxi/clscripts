01606e1f6116d8bc97c3cac2a52c534f93a1903f
maxarchx <maxisoft.it@gmail.com>
2018-06-01 13:09:27 +0500

    fix

---
+++ ftp_setup.sh
@@ -0,0 +1,25 @@
+#/bin/sh
+if [ "$EUID" -ne 0 ]
+  then echo "ОШИБКА: нужны права root!"
+  exit
+fi
+echo "Установим vsftpd"
+emerge vsftpd
+
+echo "Создадим конфигу"
+echo "anonymous_enable=NO
+local_enable=YES
+write_enable=YES
+anon_mkdir_write_enable=NO
+dirmessage_enable=YES
+xferlog_enable=YES
+connect_from_port_20=YES
+listen=YES" > /etc/vsftpd/vsftpd.conf
+
+echo "Создаем папку /ftpfiles"
+mkdir /ftpfiles
+chown ftp /ftpfiles
+chmod oug+rwx /ftpfiles
+
+
+
