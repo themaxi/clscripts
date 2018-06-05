a46544f9a52aa847f12ea769bc5b157594c7c68c
maxarchx <maxisoft.it@gmail.com>
2018-06-05 11:29:20 +0500

    fix

--- cons_setup.sh
+++ cons_setup.sh
@@ -4,6 +4,15 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo "Установка КонсультантПлюс"
+read -e -p "Путь к общей папке с К+: " -i "//sdc02/Consultant" conssmb
+read -e -p "Путь к папке монтирования: " -i "/mnt/cons" localmount
+read -e -p "Имя пользователя: " -i "melehin-mi" username
+read -e -p "Пароль: " -i "" userpwd
+read -e -p "Версия samba: " -i "1.0" sambaverion
 
+mkdir $localmount
+
+echo "$conssmb $localmount cifs username=$username,pass=$userpwd,dir_mode=0777,file_mode=0777,vers=$sambaverion" >> /etc/fstab
 
 mount -a
